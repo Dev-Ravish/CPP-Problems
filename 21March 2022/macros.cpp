bc584521a3b7b11a0e343cac54908538b587875d
Ravish Kumar <70026770+Dev-Ravish@users.noreply.github.com>
2022-03-22 00:03:56 +0530

    Update macros.cpp

--- 21March 2022/macros.cpp
+++ 21March 2022/macros.cpp
@@ -11,6 +11,10 @@ using namespace std;
 
 #define msg(x) #x
 
+#define max(x,y) (x>y?x:y)
+
+#define volume(r) 4/3*pi*cube(r)
+
 #ifndef pi //it will work if pi is not defined anywhere. Generally if too many vareables are defined then we can use this type of statement to redifne things and let no errors come up.
     #define pi 3.1425
 #endif
@@ -18,9 +22,17 @@ using namespace std;
 int main()
 {
 
-    double radius;
-    print<<msg("enter the value of radius:   ");
+    double radius, v1, v2;
+    print<<msg(enter the value of radius:   );
     input>>radius;
-    print<<msg(volume of the sphere is = )<<4/3*pi*cube(radius);
+    v1=volume(radius);
+    print<<msg(volume of the first sphere is = )<<v1<<endl;
+    
+    
+    print<<msg(enter the value of radius:   );
+    input>>radius;
+    v2=volume(radius);
+    print<<msg(volume of the second sphere is = )<<v2<<endl;
 
+    print<<msg(Greater volume is = )<<max(v1,v2)<<endl;
 }
